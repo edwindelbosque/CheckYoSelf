dbfde064d664a2505e9f4298756923250723ca72
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-07-26 17:20:59 -0600

    Add functionality to creating list items in sidebar and disabling button

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var tasksArray = [];
 var navBar = document.querySelector('nav');
 var sideBar = document.querySelector('aside');
 
@@ -10,7 +11,7 @@ function handleNav(e) {
   if (e.target.id === 'input-search') {
     console.log(document.querySelector('#input-search').value);
   }
-}
+};
 
 function handleSideBarInputs(e) {
   e.preventDefault();
@@ -18,22 +19,39 @@ function handleSideBarInputs(e) {
     console.log(document.querySelector('#input-title').value);
   }
   else if (e.target.id === 'input-item') {
+    enableButton();
     console.log(document.querySelector('#input-item').value);
   }
-}
+};
 
 function handleSideBarButtons(e) {
   e.preventDefault();
   if (e.target.id === 'button-add') {
-    console.log(e);
+    console.log("add item!");
+    addTask();
   }
   else if (e.target.id === 'button-create') {
-    console.log(e);
+    console.log('create!');
   }
   else if (e.target.id === 'button-clear') {
-    console.log(e);
+    console.log('clear!');
   }
   else if (e.target.id === 'button-filter') {
-    console.log(e);
+    console.log('filter!');
   }
+};
+
+
+function addTask() {
+  var taskItem = document.querySelector('#input-item').value;
+  var listItems = document.querySelector('#list-items');
+  listItems.insertAdjacentHTML('afterbegin', `
+    <li>${taskItem}</li>`);
+  document.querySelector('#input-item').value = '';
+  enableButton();
+};
+
+function enableButton() {
+  document.querySelector('#button-add').disabled = !document.querySelector('#input-item').value
+  console.log("hi");
 }
