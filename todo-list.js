1c42cf1b4c59afeedc6b386363d044bb0aa9cfb2
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-07-26 12:12:47 -0600

    Set up todo.list.js file

---
+++ todo-list.js
@@ -0,0 +1,24 @@
+class ToDoList {
+  constructor() {
+    this.id = id;
+    this.title = "";
+    this.urgent = false;
+    this.tasks = [];
+  }
+
+  saveToStorage() {
+
+  }
+
+  deleteFromStorage() {
+
+  }
+
+  updateToDo() { // title and urgency
+
+  }
+
+  updateTask() { // update content
+
+  }
+}
